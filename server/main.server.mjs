7b8b7b0da4f3c708afde6cb78cf3630ba7625d57
Jocelyn Llamas <mjlldlt@hotmail.com>
2024-11-12 13:22:03 -0600

    Auto-generated commit

---
+++ server/main.server.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{a}from"./chunk-6XS7UPPG.mjs";import"./chunk-NF5JTVWL.mjs";import"./chunk-DAMDP6TT.mjs";import"./chunk-65BV2RCM.mjs";import"./chunk-E44LSF4W.mjs";export{a as default};
